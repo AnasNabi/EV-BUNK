06b8f9bffd0ca04ac15c5098058a75ac14a6aa86
sclutch <anasnabi27@gmail.com>
2022-08-14 18:04:53 +0530

    major 0.4.0

--- script.js
+++ script.js
@@ -45,4 +45,15 @@ $(".option").click(function () {
   }
 
 
-                                                     
+
+  
+ 
+  function openPopup(){
+    let popup=document.getElementById("popup");
+    popup.classList.add("open-popup");
+    }
+
+    function closePopup(){
+      let popup=document.getElementById("popup");
+    popup.classList.remove("open-popup");
+    }
